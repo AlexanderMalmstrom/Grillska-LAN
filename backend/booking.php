3d91632903db2e3f4df6d4398eb2acdb59e0c9d0
Alexander Malmström <jag@alexandermalmstrom.se>
2016-02-03 14:49:12 +0100

    kollar om det finns POST data

--- backend/booking.php
+++ backend/booking.php
@@ -1,14 +1,26 @@
 <?php
-  ini_set('display_errors', "1");
+    //Display errors for debug purposes
+    ini_set('display_errors', "1");
 
-  $name = $_POST["name"];
-  $nick = $_POST["nick"];
-  $parentName = $_POST["parentName"];
-  $parentNumber = $_POST["parentNumber"];
-  $seat = $_POST["seat"];
-  echo "$name <br>";
-  echo "$nick <br>";
-  echo "$parentName <br>";
-  echo "$parentNumber <br>";
-  echo "$seat";
+    //Checks if there is any POST data
+    if(empty($_POST))
+    {
+        echo "No data";
+        die;
+    }
+
+    //Turn all POST data to PHP variables for easier use
+    $name = $_POST["name"];
+    $nick = $_POST["nick"];
+    $parentName = $_POST["parentName"];
+    $parentNumber = $_POST["parentNumber"];
+    $seat = $_POST["seat"];
+
+
+    //Print all POST for debug purposes
+    echo "$name <br>";
+    echo "$nick <br>";
+    echo "$parentName <br>";
+    echo "$parentNumber <br>";
+    echo "$seat";
   ?>
