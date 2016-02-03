00c42aabe182b609c06da67329c4eb7f36cbd524
Alexander Malmström <jag@alexandermalmstrom.se>
2016-02-03 14:50:46 +0100

    Fixade stol nummer

--- js/booking.js
+++ js/booking.js
@@ -7,29 +7,29 @@ function seats() {
     }
     document.getElementById("seats1-8").innerHTML = seats1_8;
 
-    //Platser 8-16
-    var seats8_16 = "";
+    //Platser 9-16
+    var seats9_16 = "";
     var i;
-    for (i = 9; i < 17; i++) {
-        seats8_16 += '<input type="radio" name="seat" value="' + i + '">';
+    for (i = 9; i <= 16; i++) {
+        seats9_16 += '<input type="radio" name="seat" value="' + i + '">';
     }
-    document.getElementById("seats8-16").innerHTML = seats8_16;
+    document.getElementById("seats9-16").innerHTML = seats9_16;
 
-    //Platser 17-23
-    var seats17_23 = "";
+    //Platser 17-24
+    var seats17_24 = "";
     var i;
-    for (i = 17; i < 25; i++) {
-        seats17_23  += '<input type="radio" name="seat" value="' + i + '">';
+    for (i = 17; i <= 24; i++) {
+        seats17_24  += '<input type="radio" name="seat" value="' + i + '">';
     }
-    document.getElementById("seats17-23").innerHTML = seats17_23;
+    document.getElementById("seats17-24").innerHTML = seats17_24;
 
     //Platser 26-32
-    var seats26_32 = "";
+    var seats25_32 = "";
     var i;
-    for (i = 25; i < 33; i++) {
-        seats26_32 += '<input type="radio" name="seat" value="' + i + '">';
+    for (i = 25; i <= 32; i++) {
+        seats25_32 += '<input type="radio" name="seat" value="' + i + '">';
     }
-    document.getElementById("seats24-32").innerHTML = seats26_32;
+    document.getElementById("seats25-32").innerHTML = seats25_32;
 }
 
     document.getElementById("age").onchange = function() {
@@ -37,5 +37,7 @@ function seats() {
 
         if (document.getElementById("age").checked){
             document.getElementById("parentConsent").style.display = "inline";
+            document.getElementById("parentName").setAttribute("required");
+            document.getElementById("parentNumber").setAttribute("required");
         }
 };
