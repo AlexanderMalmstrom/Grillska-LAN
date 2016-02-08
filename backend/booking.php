064b3398aa20cc7e4d0badf41a0480fd4c9b846e
Alexander Malmström <jag@alexandermalmstrom.se>
2016-02-08 10:57:48 +0100

    Quick fix för LAN

--- backend/booking.php
+++ backend/booking.php
@@ -1,6 +1,7 @@
 <?php
     //Display errors for debug purposes
     ini_set('display_errors', "1");
+    $file = 'booking.txt';
 
     //Checks if there is any POST data
     if(empty($_POST))
@@ -26,5 +27,18 @@
     echo "Parent number: $parentNumber <br>";
     echo "Seats: $seats";
 
-    mail("jag@alexandermalmstrom.se");
+
+    $msg = " Name: $name \n Email: $mail \n Parent name: $parentName \n Parent number: $parentNumber \n Seats: $seats \n \n \n";
+    $headers .= 'From: <noreply@grillskagaming.org>' . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $msg = wordwrap($msg,70);
+    $msgToMail = "Tack för att du vill komma och LANa! \n du har reserverat $seats platser";
+    $msgToMail = wordwrap($msgToMail,70);
+    $msg = utf8_encode($msg);
+
+    //mail("jag@alexandermalmstrom.se", "Booking", $msg, $headers);
+    file_put_contents($file, $msg, FILE_APPEND | LOCK_EX);
+
+    mail($mail, "Bokning", $msgToMail, $headers);
+     echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../index.html">';
   ?>
